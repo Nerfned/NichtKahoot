17f30784db8f74528fa65983337b693e3233f8c9
Asmember <76887950+Asmember@users.noreply.github.com>
2023-12-05 22:18:09 +0100

    Basic Basics

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
-import random
 from string import ascii_uppercase
+import random
+import json
+import re
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "048133dd-9e31-493e-a968-b750d81f5848"
@@ -25,53 +27,98 @@ def generate_unique_code(length):
     
     return code
 
-
+#################### Routs
 
 @app.route("/admin", methods=["POST", "GET"])
 def admin():
+    sendcontent = None
+
     adminroom = session.get("adminroom")
 
     if adminroom is None or adminroom not in adminrooms:
         return redirect(url_for("home"))
     
+    room = adminrooms[adminroom]
+    currentquestion = rooms[room]["currentquestion"]
+    users = rooms[room]["members"]
+    
     if request.method == "POST":
-        questions = session.get("questions")
-        commit = session.get("commit", False)
-        kick = session.get("kick", False)
-        previous = session.get("questions", False)
-        result = session.get("questions", False)
-        next = session.get("questions", False)
+        questions = request.form.get("questions")
+        commit = request.form.get("commit", False)
+        kick = request.form.get("kick", False)
+        previous = request.form.get("previous", False)
+        result = request.form.get("results", False)
+        next = request.form.get("next", False)
+
+
+        questions = re.sub('\s+',' ',questions)
 
         if next != False:
-            rooms[adminrooms[adminroom]]["currentquestion"] += 1
+            if currentquestion is None:
+                return render_template("admin.html", admincode=adminroom, usercode=room, questions=questions, currentquestion=currentquestion, users=users, error="Keine Fragen eingetragen")
+            
+            rooms[room]["currentquestion"] += 1
 
-            send({
-                "currentquestion": currentquestion
-                }, to=room)
+            sendcontent = {"content": {"currentquestion": currentquestion}, "to": room}
+            
+        if previous != False:
+            if currentquestion is None:
+                return render_template("admin.html", admincode=adminroom, usercode=room, questions=questions, currentquestion=currentquestion, users=users, error="Keine Fragen eingetragen")
 
-        pass
+            rooms[room]["currentquestion"] -= 1
 
-    roomcode = adminrooms[adminroom]
-    questions = rooms[roomcode]["questions"]
+            sendcontent = {"content": {"currentquestion": currentquestion}, "to": room}
+            
+        if commit != False:
+            rooms[room]["questions"] = questions
+            
+            if rooms[room]["currentquestion"] is None:
+                rooms[room]["currentquestion"] = 0
 
-    currentquestion = rooms[roomcode]["currentquestion"]
+            question = rooms[room]["questions"]
+            questionobj = json.loads(question)
 
-    users = rooms[roomcode]["members"]
+            socketio.emit("updateQuestions", questionobj[rooms[room]["currentquestion"]], to=room)
 
-    return render_template("admin.html", admincode=adminroom, usercode=roomcode, questions=questions, currentquestion=currentquestion, users=users)
+    questions = rooms[room]["questions"]
+  
+    tempusers = []
+    for name, score in users.items():
+        tempusers.append({"name": name, "score": score["score"] })
+
+    tempusers
+    return render_template("admin.html", admincode=adminroom, usercode=room, questions=questions, currentquestion=currentquestion, users=tempusers)
 
 @app.route("/quiz", methods=["POST", "GET"])
 def quiz():
-    if request.method == "POST":
-        name = request.form.get("name")
-        roomcode = request.form.get("code")
-        join = request.form.get("join", False)
-        create = request.form.get("create", False)
+    roomcode = session.get("room")
+    name = session.get("name")
 
-    return render_template("quiz.html", question=rooms[roomcode])
+    if roomcode not in rooms:
+       return redirect(url_for("home"))
+    
+    currentroom = rooms[roomcode]
+
+    currentquestion = currentroom["currentquestion"]
+
+    # if request.method == "POST":
+    #     name = request.form.get("name")
+    #     roomcode = request.form.get("code")
+    #     join = request.form.get("join", False)
+    #     create = request.form.get("create", False)
+    #     pass
+
+    if currentquestion is None:
+        return render_template("home.html", error = "Room is not ready to start", code=roomcode, name=name)
+
+    question = currentroom["questions"]
+    questionobj = json.loads(question)
+
+    return render_template("quiz.html", question=questionobj[currentquestion])
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    sessiontoken = session.get("sessiontoken")
     session.clear()
     if request.method == "POST":
         name = request.form.get("name")
@@ -91,8 +138,8 @@ def home():
             adminroom = generate_unique_code(16)
             
             rooms[room] = {
-                "members": [],
-                "currentquestion": 0,
+                "members": {},
+                "currentquestion": None,
                 "questions": []
                 }
             adminrooms[adminroom] = room
@@ -106,46 +153,56 @@ def home():
             session["adminroom"] = adminroom
             return redirect(url_for("admin"))
         
+        if name in rooms[room]["members"]:
+            if sessiontoken != rooms[room]["members"][name]["sessiontoken"]:
+                return render_template("home.html", error = "Username already taken", code=code, name=name)
+        else:
+            session["sessiontoken"] = generate_unique_code(32)
+
         session["name"] = name
-        session["sessiontoken"] = generate_unique_code(32)
         session["room"] = room
 
         return redirect(url_for("quiz"))
 
     return render_template("home.html")
 
-@app.route("/room")
-def room():
-    room = session.get("room")
 
-    if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("home"))
+# @app.route("/room")
+# def room():
+#     room = session.get("room")
 
-    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+#     if room is None or session.get("name") is None or room not in rooms:
+#         return redirect(url_for("home"))
 
-@socketio.on("message")
-def message(data):
-    room = session.get("room")
+#     return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+
+############################################ Web Sockets
+
+# @socketio.on("message")
+# def message(data):
+#     room = session.get("room")
     
-    if room not in rooms:
-        return
+#     if room not in rooms:
+#         return
     
-    name = session.get("name")
-    ddata = data["data"]
+#     name = session.get("name")
+#     ddata = data["data"]
 
-    content = {
-        "name": name,
-        "message": ddata
-    }
+#     content = {
+#         "name": name,
+#         "message": ddata
+#     }
 
-    send(content, to=room)
-    rooms[room]["messages"].append(content)
-    print(f"{name} said: {ddata}")
+#     send(content, to=room)
+#     rooms[room]["messages"].append(content)
+#     print(f"{name} said: {ddata}")
 
 @socketio.on("connect")
 def connect(auth):
     room = session.get("room")
     name = session.get("name")
+    adminroom = session.get("adminroom")
     
     if room is None or name is None:
         return
@@ -154,26 +211,39 @@ def connect(auth):
         leave_room(room)
         return
     
-    join_room(room)
-    send({"name": name, "message": "has entered the room"}, to=room)
-    rooms[room]["members"] += 1
-    print(f"{name} joined room {room}")
+    if adminroom is not None:
+        join_room(adminroom)
+    else:
+        join_room(room)
+        # send({"name": name, "score": 0}, to=room)
+        socketio.emit("userJoin", {"name": name, "score": 0}, to=adminroom)
+
+        rooms[room]["members"][name] = {"sessiontoken": session.get("sessiontoken"),"score": 0}
+
+        print(f"{name} joined room {room}")
 
 @socketio.on("disconnect")
 def disconnect():
     room = session.get("room")
+    adminroom = session.get("adminroom")
     name = session.get("name")
 
-    leave_room(room)
+    if adminroom is None:
+        leave_room(room)
+        if room in rooms:
+            del rooms[room]["members"][name]
+            socketio.emit("userLeve", {"name": name}, to=adminroom)
+            # if len(rooms[room]["members"]) <= 0:
+            #     del rooms[room]
+            #     print(f"deleted room {room}")
+    else:
+        leave_room(adminroom)
 
-    if room in rooms:
-        rooms[room]["members"] -= 1
-        if rooms[room]["members"] <= 0:
-            del rooms[room]
-            print(f"deleted room {room}")
 
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left room {room}")
+
+########### Main
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
